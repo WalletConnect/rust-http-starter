7e2bf54af108bf14db02f580b6a271f36ada3913
Derek <alexanderderekrein@gmail.com>
2023-01-24 00:44:00 +0400

    fix: terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,8 +37,8 @@ resource "aws_prometheus_workspace" "prometheus" {
 module "o11y" {
   source = "./monitoring"
 
-  environment = terraform.workspace
-  app_name = local.app_name
+  environment             = terraform.workspace
+  app_name                = local.app_name
   prometheus_workspace_id = aws_prometheus_workspace.prometheus.id
 }
 
